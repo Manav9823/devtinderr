ad334dc1e0ca923d187aae53efc3aefdbeecc41f
Manav9823 <manavbansalfddk@gmail.com>
2025-02-06 11:01:58 +0530

    checked location before sending the code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,8 @@
-export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+// export const BASE_URL =
+//   location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+
+
+  export const BASE_URL =
+  typeof window !== "undefined" && window.location.hostname === "localhost"
+    ? "http://localhost:7777"
+    : "/api";
